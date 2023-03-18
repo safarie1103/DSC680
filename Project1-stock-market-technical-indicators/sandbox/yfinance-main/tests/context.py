a6565798558a5900eb6d97dc0839ff1c47d0a9ad
safarie1103 <54446804+safarie1103@users.noreply.github.com>
2023-03-18 17:28:26 -0500

    03182023

--- Project1-stock-market-technical-indicators/sandbox/yfinance-main/tests/context.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import sys
-import os
-_parent_dp = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-_src_dp = _parent_dp
-sys.path.insert(0, _src_dp)
-
-import yfinance
